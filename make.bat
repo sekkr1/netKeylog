2ab9c52009a1df8eaa58ce0458a4120c7bc341c0
Dekel Reches <dekel1.reches@gmail.com>
2018-03-02 05:16:32 +0200

    mobed spec file

--- make.bat
+++ make.bat
@@ -5,9 +5,9 @@ mkdir build
 "./bin/openssl.exe" req -x509 -newkey rsa:4096 -keyout build/key.pem -out build/cert.pem -days 24855 -nodes -config bin/openssl.cnf
 
 echo COMPLILING CLIENT...
-pyinstaller src/client.py --onefile --add-data build/cert.pem;. --noconsole --clean
+pyinstaller src/client.py --specpath build/  --onefile --add-data build/cert.pem;. --noconsole --clean
 
 echo COMPILING CONTROLLER...
-pyinstaller src/controller.py --onefile --add-data build/cert.pem;. --add-data build/key.pem;. --noconsole --clean
+pyinstaller src/controller.py --specpath build/ --onefile --add-data build/cert.pem;. --add-data build/key.pem;. --noconsole --clean
 
 echo DONE. EXECUTABLES IN DIST/
